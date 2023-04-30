7657cb27be59c17b21d4f1b23d5d7f90cb6c7dad
mossaddak <mossaddak15-2413@diu.edu.bd>
2023-05-01 01:36:20 +0600

    updated

--- core/settings.py
+++ core/settings.py
@@ -152,38 +152,6 @@ SIMPLE_JWT = {
 AUTH_USER_MODEL = 'user_profile.User'
 
 
-
-
-#password reset purpose
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'#oke
-# EMAIL_HOST = 'smtp.gmail.com'#oke
-
-# EMAIL_PORT = '587'#oke
-# EMAIL_HOST_USER = 'donotreply@rvk.in'#leave here your genuine email
-# EMAIL_HOST_PASSWORD = 'Tpy@475631'#leave here your genuine password of your email. keep it in mind, as the password should in encrypted condition 
-# EMAIL_USE_TLS = True#oke
-
-
-#password reset purpose
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'#oke
-EMAIL_HOST = 'smtp.gmail.com'#oke
-
-EMAIL_PORT = '587'#oke
-EMAIL_HOST_USER = ''#leave here your genuine email
-EMAIL_HOST_PASSWORD = 'zqrgvttkmlpxuqjy'#leave here your genuine password of your email. keep it in mind, as the password should in encrypted condition 
-EMAIL_USE_TLS = True#oke
-
-
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'#oke
-# #EMAIL_HOST = 'smtp.gmail.com'#oke
-# EMAIL_HOST = 'mail.mossaddak.in'
-
-# EMAIL_PORT = '465'#oke
-# EMAIL_HOST_USER = 'donotreply@rvk.in'#leave here your genuine email
-# EMAIL_HOST_PASSWORD = 'Tpy@475631'#leave here your genuine password of your email. keep it in mind, as the password should in encrypted condition 
-# EMAIL_USE_SSL = True#oke
-
-
 TIME_ZONE = 'Asia/Dhaka'
 
 
